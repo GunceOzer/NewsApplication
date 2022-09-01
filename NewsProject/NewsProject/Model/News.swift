0119d9fd78cd0ea280ce3197c925d96be8acb5f4
Günce Demirel ÖZER <44786287+GunceOzer@users.noreply.github.com>
2022-09-01 16:16:24 +0300

    Add files via upload

---
+++ NewsProject/NewsProject/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  NewsProject
+//
+//  Created by Günce Özer on 1.09.2022.
+//
+
+import Foundation
+
+struct News : Decodable{
+    
+    let title : String
+    let story : String
+    
+}
